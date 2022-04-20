65e15d830069bd48326067cac46ce910ef0660ff
gurmukhdhillon <gurmukhsingh@intellify.com.au>
2022-04-20 21:38:11 +1000

    updated for env condition

--- project1/models/postgres/dest_table.sql
+++ project1/models/postgres/dest_table.sql
@@ -1,5 +1,7 @@
-
-
+{% if target.name == 'dev' %}
+  -- just make a view that points to the prod model
+  {{ config(materialized='view') }}
+{% endif %}
 
 with source_employee_table as (
     select * from {{ source('source_table','employee')}}
